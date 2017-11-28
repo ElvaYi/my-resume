1ed29ec64e64fc6b213f24a3c37a53927d6b7c23
Elva Yi <ElvaYi@example.com>
2017-11-28 00:55:06 -0500

    edit professor and course

--- manage.py
+++ manage.py
@@ -1,12 +1,30 @@
 from flask_script import Manager
-from resume import app
+from resume import app, db, Professor, Course
 
 manager = Manager(app)
 
 
+# reset the database and create two artists
 @manager.command
-def hello():
-    print "hello"
+def deploy():
+    db.drop_all()
+    db.create_all()
+    Terry = Professor(name='Terry', department='Finance department')
+    Harry = Professor(name='Harry', department='ACCT and MISY department')
+    Richard = Professor(name='Richard', department='Finance Department')
+    finc415 = Course(coursenumber='FINC415', title='International Finance', professor_id=1, description='Examines the international monetary environment and its impact on financial planning for the firm. Topics include exchange rates, currency restrictions, tax regulations, direct investment theory, capital budgeting, financing, risk management, and working capital management')
+    misy350 = Course(coursenumber='MISY350', title='Business Application Development', professor_id=2, description='Covers concepts related to client side development, including cascading style sheets and JavaScript')
+    finc311 = Course(coursenumber='FINC311', title='Principle of Finance', professor_id=3, description='Introduces fundamental techniques and concepts related to the financial management of business firms. Topics include the time value of money, valuation, capital budgeting, working capital management, cost of capital, capital structure analysis, short and long term financing')
+    finc419 = Course(coursenumber='FINC419', title='Financial Modeling', professor_id=3, description='Applies economic principles and financial modeling techniques to value seasoned equity, initial public offerings, mergers, private equity transactions, and leveraged buyouts')
+    db.session.add(Terry)
+    db.session.add(Harry)
+    db.session.add(Richard)
+    db.session.add(finc415)
+    db.session.add(misy350)
+    db.session.add(finc311)
+    db.session.add(finc419)
+    db.session.commit()
+    db.session.commit()
 
 
 if __name__ == "__main__":
--- resume.py
+++ resume.py
@@ -1,5 +1,33 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, session, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess'
+
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+
+# define database tables
+class Professor(db.Model):
+    __tablename__ = 'professors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    department = db.Column(db.Text)
+    courses = db.relationship('Course', backref='professor')
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    coursenumber = db.Column(db.String(256))
+    title = db.Column(db.String(256))
+    description = db.Column(db.Text)
+    professor_id = db.Column(db.Integer, db.ForeignKey('professors.id'))
 
 
 @app.route('/')
@@ -9,14 +37,86 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/professors')
+def show_all_professors():
+    professors = Professor.query.all()
+    return render_template('professor-all.html', professors=professors)
+
+
+@app.route('/professor/add', methods=['GET', 'POST'])
+def add_professors():
+    if request.method == 'GET':
+        return render_template('professor-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        department = request.form['department']
+
+        # insert the data into the database
+        professor = Professor(name=name, department=department)
+        db.session.add(professor)
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
+
+
+@app.route('/professor/edit/<int:id>', methods=['GET', 'POST'])
+def edit_professor(id):
+    professor = Professor.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('professor-edit.html', professor=professor)
+    if request.method == 'POST':
+        # update data based on the form data
+        professor.name = request.form['name']
+        professor.department = request.form['department']
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
+
+
+# course-all.html adds song id to the edit button using a hidden input
 @app.route('/courses')
 def show_all_courses():
-    courses = [
-        'MISY350',
-        'FINC419',
-        'FINC415'
-      ]
+    courses = Course.query.all()
     return render_template('course-all.html', courses=courses)
+
+
+@app.route('/course/add', methods=['GET', 'POST'])
+def add_courses():
+    if request.method == 'GET':
+        professors = Professor.query.all()
+        return render_template('course-add.html', professors=professors)
+    if request.method == 'POST':
+        # get data from the form
+        coursenumber = request.form['coursenumber']
+        title = request.form['title']
+        description = request.form['discription']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course = Course(coursenumber=coursenumber, title=title, description=description, professor=professor)
+
+    # insert the data into the database
+    db.session.add(course)
+    db.session.commit()
+    return redirect(url_for('show_all_courses'))
+
+
+@app.route('/course/edit/<int:id>', methods=['GET', 'POST'])
+def edit_course(id):
+    course = Course.query.filter_by(id=id).first()
+    professors = Professor.query.all()
+    if request.method == 'GET':
+        return render_template('course-edit.html', course=course, professors=professors)
+    if request.method == 'POST':
+        # update data based on the form data
+        course.coursenumber = request.form['coursenumber']
+        course.title = request.form['title']
+        course.description = request.form['description']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course.professor = professor
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_courses'))
 
 
 @app.route('/about')
@@ -37,10 +137,25 @@ def get_user_name(name):
     return render_template('user.html', name=name)
 
 
-@app.route('/song/<int:id>/')
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    # how to get form data is different for GET vs. POST
+    if request.method == 'GET':
+        first_name = request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html', first_name=first_name)
+        else:
+            return render_template('form-demo.html', first_name=session.get('first_name'))
+    if request.method == 'POST':
+        session['first_name'] = request.form['first_name']
+        # return render_template('form-demo.html', first_name=first_name)
+        return redirect(url_for('form_demo'))
+
+
+@app.route('/course/<int:id>/')
 def get_song_id(id):
     # return "This song's ID is " + str(id)
-    return "Hi, this is %s and the song's id is %d" % ('administrator', id)
+    return "Hi, this is %s and the course's id is %d" % ('administrator', id)
 
 
 # https://goo.gl/Pc39w8 explains the following line
